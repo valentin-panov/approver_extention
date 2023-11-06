6102cd03ddf2e30f7ef55dd9ae07b256798dfee7
Valentin Panov <valentin.panov@me.com>
2023-11-06 14:51:37 +0100

    1.0.2

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // Clean approve comments
 
+const text =
+  "Approved. The number of code or files has changed due to the merge - ";
+
 // Cleaning function
 function cleanInput({ target }) {
   // Use regex to remove symbols except letters, numbers, spaces, and dashes
   // Update the textarea with the cleaned value
   const regex = /[^a-z0-9\s-]/gim;
-  target.value = target.value
-    .replace(regex, " ")
-    .replace(/ +(?= )/g, "")
-    .trim();
+  target.value =
+    text +
+    target.value
+      .replace(regex, " ")
+      .replace(/ +(?= )/g, "")
+      .trim();
   target.removeEventListener("input", cleanInput);
   target.dispatchEvent(new Event("input", { bubbles: true }));
 }
@@ -36,8 +41,7 @@ function waitForTextArea() {
 }
 
 function handleTextArea(textArea) {
-  textArea.value =
-    "Approved. The number of code/files has changed due to the merge/new library:";
+  textArea.placeholder = text;
   textArea.addEventListener("input", cleanInput);
 }
 
